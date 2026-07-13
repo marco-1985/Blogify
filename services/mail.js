7ae9c10b68141e9a59f9945a42e2e65dbc30aa85
Karan Kumar <karank47417@gmail.com>
2026-07-13 13:20:03 +0530

    Migrate email service to Resend

--- services/mail.js
+++ services/mail.js
@@ -4,10 +4,6 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    requireTLS: true,
-    pool: true,
-    maxConnections: 5,
-    maxMessages: 100,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
